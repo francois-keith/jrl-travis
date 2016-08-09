a7ae08a19fb639dd9d771ddc0273df4fc2a7cd0c
Francois Keith <francois.keith@gmail.com>
2016-08-10 01:19:12 +0200

    Revert "[AppVeyor] Removes the env modification to make sure that git.exe is still available."
    This reverts commit 84d1393b09c9ddbd7b1f304a98df5f662e38dbc6.
    
    Conflicts:
    	functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -80,11 +80,14 @@ function install_git_dependencies
     cd build
     # For projects that use cmake_add_subfortran directory this removes sh.exe
     # from the path
+    $Env:Path = $Env:Path -replace "Git","dummy"
     cmake ../ -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="${Env:CMAKE_INSTALL_PREFIX}" -DPYTHON_BINDING=OFF -DMINGW_GFORTRAN="$env:MINGW_GFORTRAN"
     type CMakeCache.txt
     if ($lastexitcode -ne 0){ exit $lastexitcode }
     msbuild INSTALL.vcxproj /p:Configuration=Debug
     if ($lastexitcode -ne 0){ exit $lastexitcode }
+    # Reverse our dirty work
+    $Env:Path = $Env:Path -replace "dummy","Git"
   }
 }
 
@@ -102,10 +105,12 @@ function build_project
   md build
   cd build
   # See comment in dependencies regarding $Env:Path manipulation
+  $Env:Path = $Env:Path -replace "Git","dummy"
   cmake ../ -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="${Env:CMAKE_INSTALL_PREFIX}" -DPYTHON_BINDING=OFF -DMINGW_GFORTRAN="$env:MINGW_GFORTRAN"
   if ($lastexitcode -ne 0){ exit $lastexitcode }
   msbuild INSTALL.vcxproj /p:Configuration=Debug
   if ($lastexitcode -ne 0){ exit $lastexitcode }
+  $Env:Path = $Env:Path -replace "dummy","Git"
 }
 
 function test_project
